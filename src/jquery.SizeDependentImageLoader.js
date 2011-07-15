58c38b43ec4cdb49e5ba0512ab2d473184598988
Ulrik Guenther <ulrik@42degreesoffreedom.com>
2011-07-15 18:27:12 +0200

    Added background repositioning, did some cleaning up. Still buggy :(

--- src/jquery.SizeDependentImageLoader.js
+++ src/jquery.SizeDependentImageLoader.js
@@ -5,48 +5,24 @@
         this.init = function(element, options) {
             this.options = $.extend({}, $.SizeDependentImageLoader.defaultOptions, options);
             // sort the sizes array in numerical order
-            this.options.maximalVerticalSizes = 
-                this.options.maximalVerticalSizes.sort(function(a, b){return a-b});
+            this.options.maximumHorizontalSizes = 
+                this.options.maximumHorizontalSizes.sort(function(a, b){return a-b});
 
             if(this.options.findImageOnInit) {
-                windowSizeUpdated($(this), this.options);
+                windowSizeUpdated(element, this.options);
             }
 
-            $(window).bind('resize.SizeDependentImageLoader', {elem: $(this), options: this.options},
+            $(window).bind('resize.SizeDependentImageLoader', {elem: element, options: this.options},
                 function(event) { windowSizeUpdated(event.data.elem, event.data.options); }
             );
 
-            /*return this.each(function() {
-                 var $this = $(this),
-                   data = $this.data('SizeDependentImageLoader'),
-                    currentWindowWidth = $(window).width(),
-                    currentImageWidth = 0
-                 });
-         
-                // If the plugin hasn't been initialized yet
-                if ( ! data ) {
-                    $(this).data('SizeDependentImageLoader', {
-                        currentWindowWidth : $(window).width(),
-                        currentImageWidth : 0
-                    });
-                }
-            });*/
-            
-        
+            return element;
         };
 
         this.destroy = function() {
-            /*return this.each(function(){
+            return this.each(function(){
                 $(window).unbind('.SizeDependentImageLoader');
-
-                var $this = $(this),
-                    data = $this.data('SizeDependentImageLoader');
-
-                // Namespacing FTW
-                $(window).unbind('.SizeDependentImageLoader');
-                data.tooltip.remove();
-                $this.removeData('SizeDependentImageLoader');
-            });*/ 
+            }); 
         };
 
         this.init(element, options);
@@ -62,34 +38,50 @@ $.SizeDependentImageLoader.defaultOptions = {
             'imageName' : '',
             'imageExtension' : 'jpg',
             'imageBaseURL' : '',
-            'maximalVerticalSizes' : [1024, 1400, 1920],
+            'imageVerticalSizes:': [768, 900, 1200],
+            'maximumHorizontalSizes' : [1024, 1400, 1920],
             'cssProperty' : 'background-image',
-            'findImageOnInit' : true
+            'findImageOnInit' : true,
+            'repositionBackground': true,
+            'backgroundPosition': 'center'
 };
 
 function windowSizeUpdated(element, options) {
             // find best fitting window width
             // set cssProperty of bindToClass or bindToElement to
             // the best-fitting image
-            sizes = options.maximalVerticalSizes;
+            sizes = options.maximumHorizontalSizes;
+            verticalSizes = options.imageVerticalSizes;
+            windowWidth = $(window).width();
+            windowHeight = $(window).height();
+
             var i = 0;
+            var found = false;
+
             for(i = 0; i < sizes.length; i++) {
                // we have found an image exceeding the viewport width
-               if(sizes[i] > $(window).width() &&
-                  sizes[i-1] < $(window).width() ) {
-                   console.log("Found fitting size: " + sizes[i]);
-                   element.css(options.cssProperty, "url(" + options.imageBaseURL + "/" + options.imageName + "_" + sizes[i] + "." + options.imageExtension + ")");
-                   return;
+               if(sizes[i] >= windowWidth && sizes[i-1] <= windowWidth 
+                  || verticalSizes[i] >= windowHeight && verticalSizes[i-1] <= windowHeight) {
+                   found = true;
+                   break;
                }
             }
-            if(sizes[0] > $(window).width()) {
+            if(sizes[0] >= windowWidth) {
                 i = 0;
-            } if (sizes[sizes.length-1] < $(window).width()) {
+            } if (!found && sizes[sizes.length-1] < windowWidth) {
                 i = sizes.length-1;
             }
-            console.log("Found fitting size: " + sizes[i]);
+
             element.css(options.cssProperty, "url(" + options.imageBaseURL + "/" + options.imageName + "_" + sizes[i] + "." + options.imageExtension + ")");
-        };
+
+            if(options.repositionBackground) {
+                if(options.backgroundPosition == "center") {
+                    position_x = -Math.abs(sizes[i] - windowWidth)/2;
+                    position_y = -Math.abs(verticalSizes[i] - windowHeight)/2;
+                    element.css("background-position", position_x + "px " + position_y + "px"); 
+                }
+            }
+};
 
 
 })( jQuery );
